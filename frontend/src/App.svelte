3ce1d266e521bf7df18787f19141634b13ead054
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-11-02 18:07:59 -0400

    go!

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,25 @@
 <script>
   import Crank from "./components/Crank.svelte";
+  import Dial from "./components/Dial.svelte";
   import Lever from "./components/Lever.svelte";
+  let dialNumber = $state(0);
+  setInterval(() => {
+    dialNumber++;
+    if (dialNumber > 100) {
+      dialNumber = 0;
+    }
+  }, 100);
 </script>
 
 <div>
-  <Crank />
+  <!-- <Crank /> -->
 </div>
-<div>
-  <Lever />
-</div>
+
+<!-- <div> -->
+<!-- <Lever /> -->
+<!-- </div> -->
+
+<!-- <Dial size={350} bind:number={dialNumber} max={100} /> -->
 
 <style>
   div {
@@ -16,5 +27,8 @@
     justify-content: center;
     align-items: center;
     height: 50vh;
+  }
+  :global(body) {
+    background-image: url("/bg.jpg");
   }
 </style>
