63310e39c5946c35b466b85f0a97c44b8f40d8d4
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-11-03 08:36:40 -0500

    ypdate

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -19,6 +19,13 @@
   // }, 100);
 
   let coalArr = $state(new Array(10));
+  //add a random chance of coal appearing every 10 seconds
+  setInterval(() => {
+    if (Math.random() > 0.15 && coalArr.length < 10) {
+      coalArr.push("");
+    }
+  }, 10000);
+
   let coalVals_height = $state(0);
   let coalVals_width = $state(0);
   let coalVals_x = $state(0);
@@ -29,7 +36,6 @@
   $effect(() => {
     dialNumber = CrankVal;
   });
-
   let simTemp = $state(20);
   // Cond-Boiler Pressure
   let simP1 = $state(0);
@@ -60,6 +66,28 @@
       ActObserve();
     }, 1000);
   }
+  $effect(() => {
+    if (simTemp > 700) {
+      // reset the game
+      simTemp = 20;
+      simCoal = 0;
+      simP1 = 0;
+      simP2 = 0;
+      simP3 = 0;
+      simSpeed = 0;
+      actAddCoal = 0;
+      actDumpCoal = false;
+      actPanelOpen = false;
+      actExhaustOpen = 0;
+      actGear = 0;
+      actEngineIntake = 0;
+      actFurnaceIntake = 0;
+      // get a new session id
+      StartGame().then(() => {
+        window.location.href = "/fail.gif";
+      });
+    }
+  });
   async function ActObserve() {
     var postHeaders = new Headers();
     postHeaders.append("Content-Type", "application/json");
@@ -141,6 +169,9 @@
     }}
     size="350"
   />
+  <div class="poslable_1">
+    <Label label="Emergency Dump" />
+  </div>
 </div>
 <div class="cord">
   <PullCord size="200" bind:val={actExhaustOpen} />
@@ -200,6 +231,11 @@
 </div> -->
 
 <style>
+  .poslable_1 {
+    position: absolute;
+    top: 8em;
+    left: -5em;
+  }
   .help {
     position: absolute;
     top: 1em;
